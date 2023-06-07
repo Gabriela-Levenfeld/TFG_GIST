1609dd95d1e82128d39d661e7dfd40ff42dd96af
Gabriela-Levenfeld <43616917+Gabriela-Levenfeld@users.noreply.github.com>
2023-06-07 10:11:41 +0200

    Refactor: removed unused methods related to MGCN model

--- Codigo/utils/featurizers.py
+++ Codigo/utils/featurizers.py
@@ -15,8 +15,6 @@ def get_atom_featurizer(atom_featurizer):
         return AttentiveFPAtomFeaturizer()
     elif atom_featurizer == 'pretrain':
         return PretrainAtomFeaturizer()
-    elif atom_featurizer == 'alchemy':
-        return alchemy_nodes
     else:
         raise ValueError('Invalid atom featurizer')
 
@@ -28,8 +26,6 @@ def get_bond_featurizer(bond_featurizer, self_loop):
         return AttentiveFPBondFeaturizer(self_loop=self_loop)
     elif bond_featurizer == 'pretrain':
         return GINPretrainBondFeaturizer(self_loop=self_loop)
-    elif bond_featurizer == 'alchemy':
-        return alchemy_edges
     else:
         raise ValueError('Invalid bond featurizer')
 
@@ -42,39 +38,6 @@ def get_transformer(transformer):
     else:
         raise ValueError('Invalid transformer')
 
-
-# Adapted from https://lifesci.dgl.ai/_modules/dgllife/data/alchemy.html#TencentAlchemyDataset
-def alchemy_nodes(mol):
-    atom_feats_dict = defaultdict(list)
-    num_atoms = mol.GetNumAtoms()
-    for u in range(num_atoms):
-        atom = mol.GetAtomWithIdx(u)
-        atom_type = atom.GetAtomicNum()
-        atom_feats_dict['h'].append(atom_type)
-
-    atom_feats_dict['h'] = F.tensor(np.array(
-        atom_feats_dict['h']).astype(np.int64))
-
-    return atom_feats_dict
-
-
-def alchemy_edges(mol, self_loop=False):
-    bond_feats_dict = defaultdict(list)
-
-    mol_conformers = mol.GetConformers()
-    assert len(mol_conformers) == 1
-    geom = mol_conformers[0].GetPositions()
-
-    num_atoms = mol.GetNumAtoms()
-    for u in range(num_atoms):
-        for v in range(num_atoms):
-            if u == v and not self_loop:
-                continue
-            bond_feats_dict['e'].append(
-                np.linalg.norm(geom[u] - geom[v]))
-    bond_feats_dict['e'] = F.tensor(
-        np.array(bond_feats_dict['e']).astype(np.float32)).reshape(-1 , 1)
-    return bond_feats_dict
 
 
 # Class PretrainBondFeaturizer adapted for this specific dataset
