c3f1c5b082b74e4d91f75d6abe42a67fed95f10a
Gabriela-Levenfeld <43616917+Gabriela-Levenfeld@users.noreply.github.com>
2023-06-13 19:15:28 +0200

    Included bayesian search... Not working!

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,5 +1,4 @@
 import time
-import dgl
 import numpy as np
 import optuna
 import pandas as pd
@@ -7,200 +6,119 @@ import pandas as pd
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from dgllife.utils import mol_to_bigraph
 from rdkit import Chem
 
 
 from utils.data import load_alvadesc_data, load_mols_df
-from utils.featurizers import get_atom_featurizer, get_bond_featurizer, get_transformer
-from utils.memoization import memorize
 from utils.train.model_selection import stratified_train_validation_test_split
 from utils.train.param_search import param_search
 from models.GNNModel import GATv2Model, AttentiveFPModel, MPNNModel, GINModel
+from utils.graph_utils import build_graph_and_transform_target, collate_molgraphs, to_cuda
 
 
-#@memorize
-def build_graph_and_transform_target(train, validation, test, atom_alg, bond_alg, transformer_alg, self_loop):
-    (X_train, y_train) = train
-    (X_val, y_val) = validation
-    (X_test, y_test) = test
+def create_estimators():
+    estimator = [
+        ('gatv2', GATv2Model()),
+        ('attentiveFP', AttentiveFPModel()),
+        ('mpnn', MPNNModel()),
+        ('gin', GINModel())
+    ]
+    return estimator
 
-    atom_featurizer = get_atom_featurizer(atom_alg)
-    bond_featurizer = get_bond_featurizer(bond_alg, self_loop)
-    transformer = get_transformer(transformer_alg)
-
-    y_train = y_train.reshape(-1, 1)
-    y_val = y_val.reshape(-1, 1)
-    y_test = y_test.reshape(-1, 1)
-    if transformer is not None:
-        y_train = transformer.fit_transform(y_train)
-        y_val = transformer.fit_transform(y_val)
-        y_test = transformer.transform(y_test)
-
-
-    def featurize(x, y):
-        # each item is a duple of type (graph(x), y)
-        return (
-            mol_to_bigraph(x, node_featurizer=atom_featurizer,
-                           edge_featurizer=bond_featurizer,
-                           add_self_loop=self_loop),
-            y
-            )
-
-    train = [featurize(x_i, y_i) for x_i, y_i in zip(X_train, y_train)]
-    validation = [featurize(x_i, y_i) for x_i, y_i in zip(X_val, y_val)]
-    test = [featurize(x_i, y_i) for x_i, y_i in zip(X_test, y_test)]
-    return train, validation, test, transformer
-
-
-def collate_molgraphs(data):
-    assert len(data[0]) == 2, 'ooops'
-    graphs, labels = map(list, zip(*data))
-
-    bg = dgl.batch(graphs)
-    bg.set_n_initializer(dgl.init.zero_initializer)
-    bg.set_e_initializer(dgl.init.zero_initializer)
-    labels = torch.tensor(np.array(labels), dtype=torch.float32).reshape(-1, 1)
-    masks = torch.ones(labels.shape)
-    return bg, labels, masks
-
-
-def to_cuda(bg, labels, masks):
-    if torch.cuda.is_available():
-        bg = bg.to(torch.device('cuda:0'))
-        labels = labels.to('cuda:0')
-        masks = masks.to('cuda:0')
-    return bg, labels, masks
-
+def dummy_create_estimators():
+    estimator = [
+        ('gatv2', GATv2Model()),
+        ('attentiveFP', AttentiveFPModel()),
+        ('mpnn', MPNNModel()),
+        ('gin', GINModel())
+    ]
+    return estimator
 
 if __name__ == '__main__':
-    batch_size = 256
-    fp_size = 1024 #No se usa en ningún momento
-    total_epochs = 40
-    self_loop = True
-    learning_rate = 1e-3
     SEED = 129767345
     #########################
-    rt_scaler = 'robust'
-    atom_featurizer = 'canonical' #attentive_featurizer, pretrain
-    bond_featurizer = 'canonical' #attentive_featurizer, pretrain
-    #######################
 
-    #X, y = load_mols_df(n=100)
-    #X_train, X_val, X_test, y_train, y_val, y_test = stratified_train_validation_test_split(X, y, test_size=0.1, validation_size=0.2, random_state=SEED)
+    X, y = load_mols_df(n=100)
+    X_train, X_val, X_test, y_train, y_val, y_test = stratified_train_validation_test_split(X, y, test_size=0.1, validation_size=0.2, random_state=SEED)
+
+    isDUMMY = True
+    if isDUMMY:
+        print("Searching dummy configuration")
+        estimators = dummy_create_estimators()
+        n_trials = 2
+    else:
+        print("Searching configuration")
+        estimators = create_estimators
+        n_trials = 10
+
+    study = optuna.create_study(study_name='GLS_TFG',
+                                direction='minimize',
+                                storage='sqlite:///GNNPredict.db', # Esto se sobreescribe (?)
+                                load_if_exists=True)
 
     # Bayesian search
-    study = optuna.create_study(study_name=f'{model_name}',
-                                direction='minimize',
-                                storage='sqlite:///GNNPredict.db',
-                                load_if_exists=True)
-    attentiveFP_model = AttentiveFPModel()
-    n_trials = 10
-    attentiveFP_model = param_search(attentiveFP_model, train_loader, study, n_trials)
+    for estimator in estimators:
+        best_params = param_search(estimator, (X_train, y_train), (X_val, y_val), study, n_trials)
+        # best_params contiene parámetros para la construccion del grafo y parámetros para testear el modelo
+        estimator.set_params(**best_params)
 
-
-    print('Building graphs...', end='')
-    start = time.time()
-    train, validation, test, transformer = build_graph_and_transform_target(
-        (X_train, y_train),
-        (X_val, y_val),
-        (X_test, y_test),
-        atom_alg=atom_featurizer,
-        bond_alg=bond_featurizer,
-        transformer_alg=rt_scaler,
-        self_loop=self_loop
-    )
-    print(f'Done! (Ellapsed: {time.time() - start})')
-
-    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, collate_fn=collate_molgraphs)
-    val_loader = DataLoader(validation, batch_size=batch_size, shuffle=False, collate_fn=collate_molgraphs)
-    test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, collate_fn=collate_molgraphs)
-
-    # Get a sample of the graph to know the node_feat_size and the edge_feat_size
-    graph, y, masks = next(iter(test_loader))
-    """
-    # Default values for GATv2
-    reg = GATv2Model(in_feats=graph.ndata['h'].shape[1],
-                     agg_modes=["flatten", "mean"],
-                     hidden_feats=[32, 32],
-                     allow_zero_in_degree=True)
-    reg = AttentiveFPModel(node_feat_size=graph.ndata['h'].shape[1],
-                           edge_feat_size=graph.edata['e'].shape[1],
-                           num_layers=2,
-                           graph_feat_size=128,
-                           dropout=0.2)
-    """
-    # Values used for prediction without optimal searching
-    reg = MPNNModel(node_in_feats=graph.ndata['h'].shape[1],
-                    edge_in_feats=graph.edata['e'].shape[1],
-                    node_out_feats=64,
-                    edge_hidden_feats=32)
-    """
-    # Default values for GIN -> Configuración especial: Featurizer='pretrain'
-    reg = GINModel(num_node_emb_list=[120, 3],
-                   num_edge_emb_list=[6, 3],
-                   num_layers=5,
-                   emb_dim=300,
-                   dropout=0.5,
-                   readout='mean')
-    """
-
-    if torch.cuda.is_available():
-        print('using CUDA!')
-        reg = reg.cuda()
-
-    optimizer = torch.optim.Adam(reg._model.parameters(), lr=learning_rate)
-
-    train_losses = []
-    test_losses = []
-    maes = []
-    medaes = []
-    loss_criterion = F.smooth_l1_loss
-
-
-    for epoch in range(1, total_epochs + 1):
-        reg.train()
-        epoch_losses = []
-        for batch_id, (bg, labels, masks) in enumerate(train_loader):
-            bg, labels, masks = to_cuda(bg, labels, masks)
-            loss = reg.train_step(reg, bg, labels, masks, loss_criterion, optimizer)
-            epoch_losses.append(loss)
-        train_losses.append(np.mean(epoch_losses))
-
-        reg.eval()
-        with torch.no_grad():
-            epoch_losses = []
-            abs_errors = []
-            for batch_id, (bg, labels, masks) in enumerate(test_loader):
-                bg, labels, masks = to_cuda(bg, labels, masks)
-                loss, absolute_errors = reg.eval_step(reg, bg, labels, masks, loss_criterion, transformer)
-                epoch_losses.append(loss)
-                abs_errors.append(absolute_errors)
-
-        test_losses.append(np.mean(epoch_losses))
-        maes.append(
-            np.mean(np.concatenate(abs_errors))
+        print('Building graphs...', end='')
+        start = time.time()
+        _, test, transformer = build_graph_and_transform_target(
+            (X_train, y_train),
+            (X_val, y_val),
+            (X_test, y_test),
+            atom_alg=best_params['atom_featurizer'],
+            bond_alg=best_params['bond_featurizer'],
+            transformer_alg=best_params['rt_scaler'],
+            self_loop=best_params['self_loop']
         )
-        medaes.append(
-            np.median(np.concatenate(abs_errors))
-        )
+        print(f'Done! (Ellapsed: {time.time() - start})')
 
-        if epoch % 1 == 0:
-            print(f'Epoch:{epoch}, Train loss: {train_losses[-1]}, Test loss: {test_losses[-1]}, Test MEDAE: {medaes[-1]}, Test MAE: {maes[-1]}' )
+        # Evaluate the estimator on TEST set
+        test_loader = DataLoader(test, batch_size=best_params['batch_size'], shuffle=False, collate_fn=collate_molgraphs)
+        # Get a sample of the graph to know the node_feat_size and the edge_feat_size
+        graph, y, masks = next(iter(test_loader))
 
+        if torch.cuda.is_available():
+            print('using CUDA!')
+            estimator = estimator.cuda()
 
-    losses = pd.DataFrame({
-        'epoch': np.arange(len(train_losses)),
-        'train_loss': train_losses,
-        'test_loss': test_losses,
-        'medae': medaes,
-        'mae': maes
-    })
-    losses.index = losses.epoch
-    # TODO: change name depending on arguments
-    losses.to_csv('losses_GATv2.csv', index=False)
-    print('Done')
+        optimizer = best_params['optimizer']
+        total_epochs = best_params['total_epochs']
+        test_losses = []
+        maes = []
+        medaes = []
+        loss_criterion = F.smooth_l1_loss
+        for epoch in range(1, total_epochs + 1):
+            estimator.eval()
+            with torch.no_grad():
+                epoch_losses = []
+                abs_errors = []
+                for batch_id, (bg, labels, masks) in enumerate(test_loader):
+                    bg, labels, masks = to_cuda(bg, labels, masks)
+                    loss, absolute_errors = estimator.eval_step(estimator, bg, labels, masks, loss_criterion, transformer)
+                    epoch_losses.append(loss)
+                    abs_errors.append(absolute_errors)
 
-    import matplotlib.pyplot as plt
-    losses[['train_loss', 'test_loss']].plot()
-    plt.show()
+            test_losses.append(np.mean(epoch_losses))
+            maes.append(np.mean(np.concatenate(abs_errors)))
+            medaes.append(np.median(np.concatenate(abs_errors)))
+            if epoch % 1 == 0:
+                print(f'Epoch:{epoch}, Test loss: {test_losses[-1]}, Test MEDAE: {medaes[-1]}, Test MAE: {maes[-1]}')
+
+        losses = pd.DataFrame({
+            'epoch': np.arange(len(test_losses)),
+            'train_loss': test_losses,
+            'test_loss': test_losses,
+            'medae': medaes,
+            'mae': maes
+        })
+        losses.index = losses.epoch
+        # TODO: change name depending on arguments
+        losses.to_csv(f'{estimator[0]}.csv', index=False)
+        print('Done')
+
+        import matplotlib.pyplot as plt
+        losses[['train_loss', 'test_loss']].plot()
+        plt.show()
--- Codigo/utils/train/param_search.py
+++ Codigo/utils/train/param_search.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
 
 from optuna.trial import TrialState
 from sklearn.base import clone
@@ -7,11 +9,7 @@ from functools import singledispatch
 
 from models.GNNModel import GATv2Model, AttentiveFPModel, MPNNModel, GINModel
 from utils.train.loss import truncated_rmse_scorer
-
-from utils.train.model_selection import stratified_train_validation_test_split
-from utils.data import load_mols_df
-import time
-from main import build_graph_and_transform_target
+from utils.graph_utils import build_graph_and_transform_target, collate_molgraphs,to_cuda
 """""
 Parameters haven't used yet
 'weight_decay': trial.suggest_uniform('weight_decay', 0, 3e-3),
@@ -85,42 +83,105 @@ def _(estimator: GINModel, trial):
     return params
 
 
-def create_objective(estimator, scoring):
+def generate_model(estimator, train_loader):
+    # Get a sample of the graph to know the node_feat_size and the edge_feat_size
+    graph, y, masks = next(iter(train_loader))
+
+    if isinstance(estimator, GATv2Model):
+        reg = GATv2Model(in_feats=graph.ndata['h'].shape[1],
+                         hidden_feats=estimator.hidden_feats,
+                         num_heads=estimator.num_heads,
+                         feat_drops=estimator.feat_drops,
+                         attn_drops= estimator.attn_drops,
+                         alphas=estimator.alphas,
+                         residuals=estimator.residuals,
+                         allow_zero_in_degree=estimator.allow_zero_in_degree,
+                         share_weights=estimator.share_weights,
+                         agg_modes=estimator.agg_modes,
+                         predictor_out_feats=estimator.predictor_out_feats,
+                         predictor_dropout=estimator.predictor_dropout)
+    elif isinstance(estimator, AttentiveFPModel):
+        reg = AttentiveFPModel(node_feat_size=graph.ndata['h'].shape[1],
+                               edge_feat_size=graph.edata['e'].shape[1],
+                               num_layers=estimator.num_layers,
+                               graph_feat_size=estimator.graph_feat_size,
+                               dropout=estimator.dropout)
+    elif isinstance(estimator, MPNNModel):
+        reg = MPNNModel(node_in_feats=graph.ndata['h'].shape[1],
+                        edge_in_feats=graph.edata['e'].shape[1],
+                        node_out_feats=estimator.node_out_feats,
+                        edge_hidden_feats=estimator.edge_hidden_feats)
+    elif isinstance(estimator,GINModel):
+        reg = GINModel(num_node_emb_list=estimator.num_node_emb_list,
+                       num_edge_emb_list=estimator.num_edge_emb_list,
+                       num_layers=estimator.num_layers,
+                       emb_dim=estimator.emb_dim,
+                       dropout=estimator.dropout,
+                       readout=estimator.readout)
+
+    if torch.cuda.is_available():
+        print('using CUDA!')
+        reg = reg.cuda()
+    return reg
+
+def bo_train(reg, train_loader, optimizer, loss_criterion):
+    train_losses = []
+
+    reg.train()
+    epoch_losses = []
+    for batch_id, (bg, labels, masks) in enumerate(train_loader):
+        bg, labels, masks = to_cuda(bg, labels, masks)
+        loss = reg.train_step(reg, bg, labels, masks, loss_criterion, optimizer)
+        epoch_losses.append(loss)
+    train_losses.append(np.mean(epoch_losses))
+    return train_losses
+
+def bo_validation(reg, val_loader, loss_criterion, transformer):
+    reg.eval()
+    with torch.no_grad():
+        epoch_losses = []
+        abs_errors = []
+        for batch_id, (bg, labels, masks) in enumerate(val_loader):
+            bg, labels, masks = to_cuda(bg, labels, masks)
+            loss, absolute_errors = reg.eval_step(reg, bg, labels, masks, loss_criterion, transformer)
+            epoch_losses.append(loss)
+            abs_errors.append(absolute_errors)
+    return epoch_losses, abs_errors
+
+
+def create_objective(estimator, train, validation, scoring):
     estimator_factory = lambda: clone(estimator)
     def objective(trial):
         estimator = estimator_factory()
-        params = estimator.suggest_params(trial)
+        # Generate the specific hyperparameters
+        params = estimator.suggest_params(estimator, trial)
 
-        if estimator.isinstance(GINModel):
+        # Generate the graphs
+        rt_scaler = 'robust'
+        if isinstance(estimator, GINModel):
             atom_featurizer = 'pretrain'
             bond_featurizer = 'pretrain'
         else:
             atom_featurizer = trial.suggest_categorical('atom_featurizer', ['canonical', 'attentive_featurizer'])
             bond_featurizer = trial.suggest_categorical('bond_featurizer', ['canonical', 'attentive_featurizer'])
-        rt_scaler = 'robust'
-        atom_featurizer = atom_featurizer
-        bond_featurizer = bond_featurizer
-        SEED = 129767345
         self_loop = trial.suggest_categorical('self_loop', [True, False])
-        X, y = load_mols_df(n=100)
-        X_train, X_val, X_test, y_train, y_val, y_test = stratified_train_validation_test_split(X, y, test_size=0.1, validation_size=0.2, random_state=SEED)
 
-        print('Building graphs...', end='')
-        start = time.time()
-        train, validation, test, transformer = build_graph_and_transform_target(
-            (X_train, y_train),
-            (X_val, y_val),
-            (X_test, y_test),
+        (X_train, y_train), (X_val, y_val), transformer, _ = build_graph_and_transform_target(
+            train=train,
+            validation=validation,
             atom_alg=atom_featurizer,
             bond_alg=bond_featurizer,
             transformer_alg=rt_scaler,
-            self_loop=self_loop
+            self_loop= self_loop
         )
-        print(f'Done! (Ellapsed: {time.time() - start})')
+        params.update({
+            'rt_scaler': rt_scaler,
+            'atom_featurizer': atom_featurizer,
+            'bond_featurizer': bond_featurizer,
+            'self_loop': self_loop
+        })
 
-        # DataLoader (?)
-
-
+        # Generate the common hyperparameters
         optimizer_name = trial.suggest_categorical('optimizer', ['Adam', 'AdamW', 'RMSprop', 'SGD'])
         learning_rate = trial.suggest_loguniform('learning_rate', 1e-5, 1e-3)
         common_hyperparameters = {
@@ -131,20 +192,39 @@ def create_objective(estimator, scoring):
         }
         params.update(common_hyperparameters)
         estimator.set_params(**params)
-        return scoring(estimator, X, y)
+
+        # Load the dataset
+        train_loader = DataLoader((X_train, y_train), batch_size=estimator.batch_size, shuffle=True, collate_fn=collate_molgraphs)
+        val_loader = DataLoader((X_val, y_val), batch_size=estimator.batch_size, shuffle=False, collate_fn=collate_molgraphs)
+
+        # Generate the model
+        reg = generate_model(estimator, train_loader)
+
+        # Training of the model
+        train_losses = []
+        val_losses = []
+        loss_criterion = F.smooth_l1_loss
+        for epoch in range(1, estimator.total_epochs + 1):
+            train_loss = bo_train(reg, train_loader, estimator.optimizer, loss_criterion)
+            val_loss, abs_errors = bo_validation(reg, val_loader, loss_criterion, transformer)
+
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+
+        return val_losses
     return objective
 
 
-def param_search(estimator, X, y, study, n_trials, scoring=truncated_rmse_scorer):
-    objective = create_objective(estimator, scoring)
+def param_search(estimator, train, validation, study, n_trials, scoring=truncated_rmse_scorer):
+    objective = create_objective(estimator, train, validation, scoring)
     trials = [trial for trial in study.get_trials() if trial.state == TrialState.COMPLETE]
     n_trials = max(0, n_trials-len(trials))
     if n_trials > 0:
         print(f"Starting {n_trials} trials")
         study.optimize(objective, n_trials=n_trials)
     best_params = load_best_params(study)
-    estimator.set_params(**best_params)
-    return estimator
+    #estimator.set_params(**best_params)
+    return best_params
 
 
 def load_best_params(study):
